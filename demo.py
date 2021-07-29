f7ae6d25455cf69614e55d6f204500b3b98f4baf
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-07-29 08:10:56 +0530

    Python Problems

--- demo.py
+++ demo.py
@@ -77,3 +77,35 @@
 
 # num=[hel()]
 # import math as m
+
+
+n = int(input())
+incomeList = [int(input()) for _ in range(n)]
+childrensList = [int(input()) for _ in range(n)]
+
+total = 0
+count = 0
+for i in range(n):
+    if(childrensList[i] > 2):
+        total += incomeList[i]
+        count+=1
+
+print(f"Avg income is : {total//count}")
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
